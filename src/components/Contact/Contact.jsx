99dc5aeace3b7cc82a23785b52017f59433d6d79
Sejal Nalawade <136812085+SejalNalawade25@users.noreply.github.com>
2024-05-11 12:54:07 +0530

    Contact.jsx

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -15,7 +15,7 @@ const Contact = () => {
     setResult("Sending....");
     const formData = new FormData(event.target);
 
-    formData.append("access_key", "YOUR ACCSSES KEY");
+    formData.append("access_key", "YOUR_ACCSSES_KEY");
 
     const response = await fetch("https://api.web3forms.com/submit", {
       method: "POST",
